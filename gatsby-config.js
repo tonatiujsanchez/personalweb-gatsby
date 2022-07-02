0f019fe28b8ab1bc3f0444d3955bb54e3268e90d
Tonatiuj Sánchez <tonsanzjimz@gmail.com>
2022-07-02 13:42:39 -0500

    Path del plugin gatsby-source-filesystem modificada

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,7 +26,8 @@ module.exports = {
       resolve: 'gatsby-source-filesystem',
       options: {
         name: 'data',
-        path: `./src/data`
+        path: `${__dirname}/src/data/`,
+        ignore: [`**/\.*`],
       }
     }
   ],
